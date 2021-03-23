5ce3702935d01ce9f7dd142cf85994b0a292c33d
Jim Capozzoli <capojame@amazon.com>
2021-03-23 10:13:12 -0400

    baby steps.

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+	document.getElementById("logoDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches(".logo-drop-button")) {
+		var dropdownElements = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var thisDropdownElement = dropdownElements[i];
+			if (thisDropdownElement.classList.contains("show")) {
+				thisDropdownElement.classList.remove("show");
+			}
+		}
+	}
+}
+
